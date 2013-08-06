6ad0da79863b9584379bf4fccefe451998c170b0
Matt Filetto <matt.filetto@gmail.com>
2013-08-06 15:38:51 -0700

    build_script: get ramdisk folder name based on platform, carrier, and mrev
    Since we have so many different branches. i'e just made ramdisk folders for
    each carrier, TW/AOP variant, and MREV variant.
    
    Change-Id: Iac3a870cac0431fab2db6bc07818a88c4d0d2da7
    
    Conflicts:
    	build_master.sh

--- build_master.sh
+++ build_master.sh
@@ -13,7 +13,7 @@ export SRC_ROOT=`readlink -f ../../..`
 export KERNELDIR=`readlink -f .`
 export PARENT_DIR=`readlink -f ..`
 export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
-export INITRAMFS_SOURCE=`readlink -f ..`/Ramdisks/$PLATFORM"_"$CARRIER
+export INITRAMFS_SOURCE=`readlink -f ..`/Ramdisks/$PLATFORM"_"$CARRIER-$MREV
 export CONFIG_$PLATFORM_BUILD=y
 export PACKAGEDIR=$KERNELDIR/Packages/$PLATFORM
 # enable ccache
